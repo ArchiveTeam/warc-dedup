483a8e069842cf0b1d8fa64387dff9dbdc4952c3
Arkiver2 <arkiver@protonmail.com>
2019-07-11 22:49:09 +0200

    Skip warc/revisit records. Add log to WARC.

--- warc_dedup/deduplicate.py
+++ warc_dedup/deduplicate.py
@@ -6,27 +6,40 @@ import warcio
 from warcio.archiveiterator import ArchiveIterator
 from warcio.warcwriter import WARCWriter
 
+from warc_dedup.log import Log
+
 
 class Warc:
     def __init__(self, warc_source: str, warc_target: str=None):
-        self._warc_source = warc_source
-        self._warc_target = warc_target or create_target_warc(warc_source)
-        if os.path.isfile(self._warc_target):
-            raise Exception('File {} already exists.'.format(self._warc_target))
+        self.warc_source = warc_source
+        self.warc_target = warc_target
         self._response_records = {}
+        self._log = Log()
+        self._log.log('Original WARC file is {}.'.format(self.warc_source))
+        self._log.log('Deduplicated WARC file is {}.'.format(self.warc_target))
+        if os.path.isfile(self.warc_target):
+            self._log.log('File {} already exists.'.format(self.warc_target))
+            raise Exception('File {} already exists.'.format(self.warc_target))
 
     def deduplicate(self):
-        with open(self._warc_source, 'rb') as s, \
-                open(self._warc_target, 'wb') as t:
-            writer = WARCWriter(filebuf=t, gzip=self._warc_target.endswith('.gz'))
+        self._log.log('Start deduplication process.')
+        with open(self.warc_source, 'rb') as s, \
+                open(self.warc_target, 'wb') as t:
+            writer = WARCWriter(filebuf=t, gzip=self.warc_target.endswith('.gz'))
             for record in ArchiveIterator(s):
                 url = record.rec_headers.get_header('WARC-Target-URI')
+                record_id = record.rec_headers.get_header('WARC-Record-ID')
+                self._log.log('Processing record {}.'.format(record_id))
                 if url is not None and url.startswith('<'):
                     url = re.search('^<(.+)>$', url).group(1)
+                    self._log.log('Replacing URL in record {} with {}.'
+                                  .format(record_id, url))
                     record.rec_headers.replace_header('WARC-Target-URI', url)
                 if record.rec_headers.get_header('WARC-Type') == 'response':
                     data = self.get_duplicate(record)
                     if data:
+                        self._log.log('Record {} is duplicate from {}.'
+                                      .format(record_id, data))
                         writer.write_record(
                             self.response_to_revisit(writer, record, data)
                         )
@@ -34,10 +47,13 @@ class Warc:
                         self.register_response(record)
                         writer.write_record(record)
                 elif record.rec_headers.get_header('WARC-Type') == 'warcinfo':
-                    record.rec_headers.replace_header('WARC-Filename', self._warc_target)
+                    self._log.set_warcinfo(record.rec_headers.get_header('WARC-Record-ID'))
+                    record.rec_headers.replace_header('WARC-Filename', self.warc_target)
                     writer.write_record(record)
                 else:
                     writer.write_record(record)
+            self._log.log('Writing log to WARC.')
+            writer.write_record(self._log.create_record(writer))
 
     def register_response(self, record):
         key = (
@@ -53,7 +69,7 @@ class Warc:
     @staticmethod
     def response_to_revisit(writer, record, data):
         warc_headers = record.rec_headers
-        if 'refers-to' in data and data['refers-to'] is not None:
+        if 'record-id' in data and data['record-id'] is not None:
             warc_headers.replace_header('WARC-Refers-To', data['record-id'])
         warc_headers.replace_header('WARC-Refers-To-Date', data['date'])
         warc_headers.replace_header('WARC-Refers-To-Target-URI',
@@ -88,13 +104,15 @@ class Warc:
         date = date.strftime('%Y%m%d%H%M%S')
         digest = record.rec_headers.get_header('WARC-Payload-Digest')
         uri = record.rec_headers.get_header('WARC-Target-URI')
-        r = requests.get('http://wwwb-dedup.us.archive.org:8083/cdx/search'
-                         '?url={}'.format(uri) + 
-                         '&limit=1'
-                         '&filter=digest:{}'.format(digest.split(':')[1]) + 
-                         '&fl=original,timestamp'
-                         '&to={}'.format(int(date) - 1))
-        print(r.url)
+        r = requests.get(
+            'http://wwwb-dedup.us.archive.org:8083/cdx/search'
+            '?url={}'.format(uri) +
+            '&limit=1'
+            '&filter=digest:{}'.format(digest.split(':')[1]) +
+            '&fl=original,timestamp'
+            '&to={}'.format(int(date) - 1) +
+            '&filter=!mimetype:warc\/revisit'
+        )
         r = r.text.strip()
         if len(r) == 0:
             return None
@@ -105,8 +123,19 @@ class Warc:
                 strftime('%Y-%m-%dT%H:%M:%SZ')
         }
 
+    @property
+    def warc_target(self) -> str:
+        return self._warc_target
 
-def create_target_warc(warc_source: str) -> str:
+    @warc_target.setter
+    def warc_target(self, value: str):
+        if value is not None:
+            self._warc_target = value
+        self._warc_target = create_warc_target(self.warc_source)
+        
+
+
+def create_warc_target(warc_source: str) -> str:
     if warc_source.endswith('.warc.gz'):
         return warc_source.rsplit('.', 2)[0] + '.deduplicated.warc.gz'
     elif warc_source.endswith('.warc'):
---
+++ warc_dedup/log.py
@@ -0,0 +1,34 @@
+import datetime
+import io
+
+
+class Log:
+    def __init__(self):
+        self._log = []
+        self._warcinfo_id = None
+
+    def log(self, value: str):
+        date = datetime.datetime.utcnow()
+        for line in value.splitlines():
+            line = line.strip()
+            self._log.append((date, line))
+
+    def create_record(self, writer):
+        payload = '\n'.join(['{} {}'.format(t.strftime('%Y-%m-%d %H:%M:%S.%f'),
+                                            s)
+                             for t, s in self._log])
+        warc_headers = {
+            'Content-Type': 'text/plain',
+        }
+        if self._warcinfo_id is not None:
+            warc_headers['WARC-Warcinfo-ID'] = self._warcinfo_id
+        return writer.create_warc_record(
+            'urn:X-warc-dedup:log',
+            'resource',
+            warc_headers_dict=warc_headers,
+            payload=io.BytesIO(payload.encode('utf8'))
+        )
+
+    def set_warcinfo(self, value: str):
+        self._warcinfo_id = value
+
