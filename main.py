2cff0df2f0a4d931cc789a397332c5c705c9df31
Arkiver2 <arkiver@protonmail.com>
2019-07-12 03:25:02 +0200

    Easy option to add output WARC filename.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from warc_dedup import deduplicate
 def main():
     if len(sys.argv) == 1:
         raise Exception('Please provide the WARC file as argument.')
-    deduplicate.Warc(sys.argv[1]).deduplicate()
+    deduplicate.Warc(*sys.argv[1:]).deduplicate()
 
 if __name__ == '__main__':
     main()
